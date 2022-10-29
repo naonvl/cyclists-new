6163524a419b11781bcb4a8e0cc44f2612dbbcf2
Nigel <delvieronigel@gmail.com>
2022-10-29 14:50:35 +0700

    Fixed patch

--- src/components/layout/canvas.tsx
+++ src/components/layout/canvas.tsx
@@ -34,7 +34,7 @@ const LCanvas: FC<CanvasProps> = ({
   ...props
 }) => {
   const canvasRenderedRef = useRef<HTMLCanvasElement>()
-  const dimensions = useStore((state) => state.dimensions)
+  const isMobileVersion = useStore((state) => state.isMobileVersion)
   const [threeProps, setThreeProps] = useState({
     camera: null,
     pointer: null,
@@ -48,54 +48,6 @@ const LCanvas: FC<CanvasProps> = ({
     initPatch({ threeProps, canvasRenderedRef, canvasRef, textureRef })
   }, [canvasRef, textureRef, threeProps])
 
-  const handleClick = useCallback(
-    (e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
-      const rect = canvasRenderedRef.current.getBoundingClientRect()
-      const array = [
-        (e.clientX - rect.left) / rect.width,
-        (e.clientY - rect.top) / rect.height,
-      ]
-      threeProps.pointer.fromArray(array)
-      // Get intersects
-      threeProps.mouse.set(
-        threeProps.pointer.x * 2 - 1,
-        -(threeProps.pointer.y * 2) + 1
-      )
-      threeProps.raycaster.setFromCamera(threeProps.mouse, threeProps.camera)
-      const intersects = threeProps.raycaster.intersectObjects(
-        threeProps.scene.children
-      )
-      // getState().updateTexture()
-
-      if (intersects.length > 0 && intersects[0].uv) {
-        let uv = intersects[0].uv
-        canvasRef.current.renderAll()
-        textureRef.current = new Texture(canvasRef.current.getElement())
-        textureRef.current.flipY = false
-        textureRef.current.needsUpdate = true
-        setState({ changed: true })
-        const positionOnScene = {
-          x: Math.round(uv.x * dimensions.width) - 4.5,
-          y: Math.round(uv.y * dimensions.height) - 5.5,
-        }
-
-        const canvasRect = canvasRef.current.getCenter()
-        // console.log(e.clientX, e.clientY)
-        // console.log({
-        //   clientX: canvasRect.left + positionOnScene.x,
-        //   clientY: canvasRect.top + positionOnScene.y,
-        // })
-        const simEvt = new globalThis.MouseEvent(e.type, {
-          clientX: canvasRect.left + positionOnScene.x,
-          clientY: canvasRect.top + positionOnScene.y,
-        })
-
-        canvasRef.current.getSelectionElement().dispatchEvent(simEvt)
-      }
-    },
-    [canvasRef, dimensions, textureRef, threeProps]
-  )
-
   return (
     <Canvas
       ref={canvasRenderedRef}
@@ -108,7 +60,9 @@ const LCanvas: FC<CanvasProps> = ({
       }}
       onMouseDown={(e) => {
         e.stopPropagation()
-        memoizedInitPatch
+        if (!isMobileVersion) {
+          memoizedInitPatch
+        }
       }}
       onCreated={({ camera, pointer, scene, raycaster, gl, mouse }) => {
         setThreeProps({ camera, pointer, scene, raycaster, gl, mouse })
